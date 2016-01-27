9178f3045ed3ff9d1917ac861a23cefcb662f4b7
Mark Lussier <mlussier@gmail.com>
2016-01-27 10:39:41 -0800

    Add route prefix enforcement, update readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #  Nodal Rate Limit Middleware
 
-This is a middleware package for [Nodal](http://www.nodaljs.com) that performs basic rate-limiting on public endpoints. This is intentionally a very simplistic piece of middleware that does not share any state.
+This is a middleware package for [Nodal](http://www.nodaljs.com) that performs basic rate-limiting on public endpoints. This is meant to be a functional example how how to write middleware for nodal and package as an npm module. Thus this is intentionally a very rudimentary and simplistic piece of middleware and my wish it others will build more advanced rate limiters.
 
 # DO NOT USE THIS YET
 This module, while totally functional, has hardcoded options. Until PR [#149](https://github.com/keithwhor/nodal/pull/149) on the main Nodal repo is merged. The module currently allows **100 connections per minute per ip**. Localhost (::1 in Nodal) is also intentionally not excluded to allow for testing.
@@ -25,15 +25,44 @@ Then simply use it
 this.middleware.use(RateLimitMiddleware)
 ```
 
+Whola your aapp is now being rate limited!.
+
+## Configuring
+
+If you want to override the default rate limiting options, you can pass configuration options when you `use()` your middleware. For example if you wnat to limit requests to 50 per every 5 minutes and allow local requests to excluded from limiting, you would do the following.
+
+```javascript
+this.middleware.use(RateLimitMiddleware, { max: 50, timeWindow: 300 * 100, exclude: ['::1'] })
+```
+
+| Optiona | Description          | Default |
+| ------------- | ----------- | ----------- |
+| timeWindow     | Time window for rate limiting in milliseconds| 60000 (1 miniute) |
+| max | Maximum number of requests in the rate limiting window | 100 requests |
+| message | Message to send back when rate limit exceeded | Too many requests, please try again later |
+| includeHeaders | Set X-RateLimit-Limit, X-Rate-Limit-Reset & X-RateLimit-Remaining headers | true|
+| exclude | Array of IPs that are excluded from rate limiting | [] |
+| enforce | Array of route prefix's to limit the application of limiting to routes | [] |
+
+
+## Apply Rate Limiting to specific routes
+Nodal currently doesn't allow middleware/renderware to be scoped to a route, thus rate limiting is applied over all your routes by default. This mens that if your building a Nodal application with both API and UI routes, it will limit your UI endpoints as well. To control the behavior you can send an optional `enforce` option that is an array of route prefixes to limit the rate limiting to. For example to limit it to only routes starting with `/v1`
+
+```javascript
+this.middleware.use(RateLimitMiddleware, { enforce: [ '/v1/' ] })
+```
+
+
 ## Headers
 
-This middleware currently sets the following headers
+This middleware can optionally (and by default doest) set the following X- headers
 
 | Name | Description          |
 | ------------- | ----------- |
 | X-RateLimit-Limit      | Request limit per minute|
 | X-RateLimit-Remaining      | The number of requests left for the time window|
 | X-Rate-Limit-Reset     | Timestamp of when the limit will be reset     |
+
 
 ## Error Messages
 
--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 'use strict';
 
 module.exports = (function() {
+
+  // Borrowed from Nodal (core/required/utilities.js)
+  function parseRegexFromString(str) {
+
+    if (str[str.length - 1] === '/') {
+      str = str.substr(0, str.length - 1);
+    }
+
+    str = str.replace(/(?:(?:\/:(\w+?)\/)|(\*))/g, (m, name, aster) => {
+      return m === '*' ? '(.*?)' : '/([^\/]+?)/';
+    });
+
+    str = str.replace(/\/:(\w+?)$/, (m, name) => {
+      return '(?:/([^\/]+?))?'
+    });
+
+    str = str.replace(/\/\(\.\*\?\)/g, '(?:\/(.*?))?');
+
+    return new RegExp(`^${str}/?$`);
+  }
+
+
   function RateLimitMiddleware(options) {
+
     this._hits = {};
     this._options = {
       timeWindow: 60 * 1000,
       max: 100,
-      message: 'Too many requests, please try again later.',
+      message: 'Too many requests, please try again later',
       includeHeaders: true,
       exclude: [
         // '::1'
+      ],
+      enforce: [
       ],
     };
 
     if (undefined !== options) {
       this._options = Object.assign({}, this._options, options);
     }
+
+    // Convert the route prefixes to regular expressions
+    this._options.enforce = this._options.enforce.map( re => parseRegexFromString(re) );
+    console.log(this._options)
 
     this._resetLimiter();
   }
@@ -36,39 +65,49 @@ module.exports = (function() {
   };
 
   RateLimitMiddleware.prototype.exec = function exec(controller, callback) {
-    const ip = controller.request().headers['x-forwarded-for'] ||
-               controller.request().connection.remoteAddress;
+    const route = controller.request().url
+    let excluded = false;
 
-    // has it been more than this._options.timeWindow?
-    if (this._reset < new Date()) {
-      this._resetLimiter();
+    this._options.enforce.forEach(re => {
+      if (!re.exec(route)) {
+        excluded = true;
+      }
+    })
+
+    if (!excluded) {
+      const ip = controller.request().headers['x-forwarded-for'] ||
+                 controller.request().connection.remoteAddress;
+
+      // has it been more than this._options.timeWindow?
+      if (this._reset < new Date()) {
+        this._resetLimiter();
+      }
+
+      if (this._options.exclude.indexOf(ip) !== -1) {
+        this._hits[ip] = 1;
+      } else if (this._hits[ip]) {
+        this._hits[ip]++;
+      } else {
+        this._hits[ip] = 1;
+      }
+
+      const reqLeft = Math.max(0, this._options.max - this._hits[ip]);
+
+      if (this._options.max && this._hits[ip] > this._options.max) {
+        controller.tooManyRequests(this._options.message, {
+          host: ip,
+          maximum: this._options.max,
+          requests: this._hits[ip],
+          resets: this._reset,
+        });
+      }
+
+      if (this._options.includeHeaders) {
+        controller.setHeader('X-RateLimit-Limit', this._options.max);
+        controller.setHeader('X-RateLimit-Remaining', reqLeft);
+        controller.setHeader('X-Rate-Limit-Reset', this._reset);
+      }
     }
-
-    if (this._options.exclude.indexOf(ip) !== -1) {
-      this._hits[ip] = 1;
-    } else if (this._hits[ip]) {
-      this._hits[ip]++;
-    } else {
-      this._hits[ip] = 1;
-    }
-
-    const reqLeft = Math.max(0, this._options.max - this._hits[ip]);
-
-    if (this._options.max && this._hits[ip] > this._options.max) {
-      controller.tooManyRequests(this._options.message, {
-        host: ip,
-        maximum: this._options.max,
-        requests: this._hits[ip],
-        resets: this._reset,
-      });
-    }
-
-    if (this._options.includeHeaders) {
-      controller.setHeader('X-RateLimit-Limit', this._options.max);
-      controller.setHeader('X-RateLimit-Remaining', reqLeft);
-      controller.setHeader('X-Rate-Limit-Reset', this._reset);
-    }
-
     callback(null);
   };
 
